fddc5164bcc2c42cd76522ff73c51be1d3791c54
wenzhengcheng <wenzhengcheng0223@163.com>
2022-05-16 00:15:32 +0800

    完善 页面逻辑

--- ruoyi-ui-studyroom/src/views/store/card/index.vue
+++ ruoyi-ui-studyroom/src/views/store/card/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="78px">
       <el-form-item label="优惠卡" prop="cardTitle">
         <el-input v-model="queryParams.cardTitle" placeholder="请输入优惠卡" clearable @keyup.enter.native="handleQuery" />
       </el-form-item>
